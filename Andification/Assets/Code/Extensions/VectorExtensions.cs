ab9c995f35744381297c4e1c994a822119460179
Enrico Ludwig <admin@sorcererslab.com>
2020-07-22 15:28:07 +0200

    Working on cell rendering

---
+++ Andification/Assets/Code/Extensions/VectorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Andification.Runtime.Extensions
+{
+    public static class VectorExtensions
+    {
+        public static void Clamp(ref this Vector3 v, Vector3 min, Vector3 max)
+        {
+            v.x = Mathf.Clamp(v.x, min.x, max.x);
+            v.y = Mathf.Clamp(v.y, min.y, max.y);
+            v.z = Mathf.Clamp(v.z, min.z, max.z);
+        }
+
+        public static void Clamp(ref this Vector2 v, Vector3 min, Vector3 max)
+        {
+            v.x = Mathf.Clamp(v.x, min.x, max.x);
+            v.y = Mathf.Clamp(v.y, min.y, max.y);
+        }
+    }
+}
--- Andification/Assets/Code/Grid/WorldGrid.cs
+++ Andification/Assets/Code/Grid/WorldGrid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Andification.Runtime.Extensions;
 
 namespace Andification.Runtime.GridSystem
 {
@@ -7,27 +8,59 @@ namespace Andification.Runtime.GridSystem
     {
         public Vector2Int worldSize = Vector2Int.zero;
         public Vector2 cellSize = Vector2.one;
+        [Space]
+        public bool drawGrid = true;
 
         private WorldGridCell[] cellMap;
 
+        private void OnDrawGizmosSelected()
+        {
+            if (drawGrid)
+                DrawEditorGrid();
+        }
+
         private void OnValidate()
         {
-            if (worldSize.x < 1)
-                worldSize.x = 1;
-
-            if (worldSize.y < 1)
-                worldSize.y = 1;
-
-            if (cellSize.x < 1)
-                cellSize.x = 1;
-
-            if (cellSize.y < 1)
-                cellSize.y = 1;
+            ClampProperties();
         }
 
         private void Awake()
         {
             InitializeGrid();
+            ClampProperties();
+        }
+
+        private void DrawEditorGrid()
+        {
+            Gizmos.color = Color.cyan;
+
+            // Horizontal Lines
+            for (int i = 0; i < worldSize.y + 1; i++)
+            {
+                Vector2 startPos = transform.position + new Vector3(transform.position.x, i * cellSize.y);
+                Vector2 endPos = transform.position + new Vector3(worldSize.x * worldSize.x, i * cellSize.y);
+
+                Gizmos.DrawLine(startPos, endPos);
+            }
+
+            // Vertical Lines
+            for (int i = 0; i < worldSize.y + 1; i++)
+            {
+                Vector2 startPos = transform.position + new Vector3(i * cellSize.x, transform.position.y);
+                Vector2 endPos = transform.position + new Vector3(i * cellSize.x, worldSize.x * worldSize.x);
+
+                Gizmos.DrawLine(startPos, endPos);
+            }
+        }
+
+        private void ClampProperties()
+        {
+            worldSize.Clamp(Vector2Int.one, Vector2Int.one * int.MaxValue);
+            cellSize.Clamp(Vector2.one, Vector2.one * float.MaxValue);
+
+            var localScale = transform.localScale;
+            localScale.Clamp(Vector3.one, Vector3.one);
+            transform.localScale = localScale;
         }
 
         private void InitializeGrid()
