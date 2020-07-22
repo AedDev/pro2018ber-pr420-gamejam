947509ca2e19d996b8f98de63b748b1b05ea64e1
teppich <johannesspicker@me.com>
2020-07-22 16:46:39 +0200

    Dissolve Noise Scale
    - scale is now adjustable, glow color pickable
    - script access granted

---
+++ Andification/Assets/VFX/Accessors/DissolveAccessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveAccessor : MonoBehaviour
+{
+	Material _material;
+
+	float _fade = 1f;
+	int scale = 6;
+	Color color;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_material = GetComponent<SpriteRenderer>()?.material;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_fade = Mathf.Abs(Mathf.Sin(Time.time));
+		SetFade(_fade);
+	}
+
+	public void SetFade(float fade) => _material.SetFloat("_Fade", fade);
+	public void SetNoiseScale(int scale) => _material.SetFloat("_NoiseScale", scale);
+	public void SetGlowColor(Color color) => _material.SetColor("_GlowColor", color);
+}
