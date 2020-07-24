2f17a86d5f47f40561ded9ebb9e081bff937c0f2
Enrico Ludwig <eludwig.work@gmail.com>
2020-07-24 17:08:35 +0200

    Changed content type of WorldGridCell to MonoBehaviour

--- Andification/Assets/Code/Runtime/Behaviours/Grid/WorldGridCell.cs
+++ Andification/Assets/Code/Runtime/Behaviours/Grid/WorldGridCell.cs
@@ -13,7 +13,7 @@ namespace Andification.Runtime.GridSystem
         [SerializeField] private bool walkable = true;
         [SerializeField] private bool buildable = true;
         [SerializeField] private GridContentType contentType = GridContentType.Nothing;
-        [NonSerialized] private GameObject content;
+        [NonSerialized] private MonoBehaviour content;
 
         [NonSerialized] private Action<WorldGridCell> cellChangedHandler;
 
@@ -74,7 +74,7 @@ namespace Andification.Runtime.GridSystem
             }
         }
 
-        public GameObject Content
+        public MonoBehaviour Content
         {
             get => content;
             set
