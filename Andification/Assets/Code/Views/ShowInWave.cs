f64cfc780c268124d5a34d2522329a8426b2ea68
Andreas Edmeier <assertores@web.de>
2020-07-24 10:21:57 +0200

    ShowInWave View

---
+++ Andification/Assets/Code/Views/ShowInWave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Andification.Runtime.View {
+	public class ShowInWave : MonoBehaviour {
+		[SerializeField] bool doShowInWave = true;
+
+		private void Start() {
+			//TODO: bind Actions
+		}
+
+		private void OnDestroy() {
+			//TODO: debind Actions
+		}
+
+		void OnSwitchToWave() {
+			gameObject.SetActive(doShowInWave);
+		}
+
+		void OnSwitchToBuild() {
+			gameObject.SetActive(!doShowInWave);
+		}
+	}
+}
+
